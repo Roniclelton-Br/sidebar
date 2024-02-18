bc99efd51c26a2471a175c542567e4e84bad42dd
Roniclelton Brito <ronicleltonbr@gmail.com>
2024-02-18 09:32:20 -0300

    projeto concluido

---
+++ script.js
@@ -0,0 +1,11 @@
+function menuMobile() {
+    const showMenuMobile = document.querySelector('.menu-mobile-active');
+
+    showMenuMobile.style.display = 'flex';
+}
+
+function closedMenuMobile() {
+    const closedMenuMobile = document.querySelector('.menu-mobile-active');
+
+    closedMenuMobile.style.display = 'none';
+}
